d604e37f6fdc612a2a2df7abfd87232a5b65c7e7
Willian Moraes <willianrm@yahoo.com.br>
2020-07-27 18:30:20 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -45,7 +45,13 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 5: seller Update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
 		
+		sellerDao.update(seller);
+		
+		System.out.println("Update completed");
 		
 		
 	}
