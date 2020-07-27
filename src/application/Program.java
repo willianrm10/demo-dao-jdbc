76b9027deeb8f0ca0532643d4bae4dbe69f7c23e
Willian Moraes <willianrm@yahoo.com.br>
2020-07-27 17:03:02 -0300

    FindAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,6 +31,14 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== TEST 3: seller findALL =====");
+				
+		list = sellerDao.findAll();
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
 		
 		
 		
