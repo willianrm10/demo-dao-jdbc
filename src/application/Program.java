0eae34cb1a325ff39418bfadf31cd9fb4eeef7b8
Willian Moraes <willianrm@yahoo.com.br>
2020-07-27 18:40:15 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,7 +12,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
 		//Department obj = new Department(1, "books");
 		//Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		
@@ -53,7 +54,12 @@ public class Program {
 		
 		System.out.println("Update completed");
 		
+		System.out.println("\n=== TEST 6: seller Delete =====");
 		
+		System.out.println("INforme um id para deletar");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
 	}
 
 }
